0dc8a5eb0eeade39c3ea7bfe03fbda37d06cc7f7
Luiz Togni <contato.togni@gmail.com>
2025-07-16 14:24:47 -0300

    feat: add flask basic config

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DB_URI")
+
+db = SQLAlchemy(app)
