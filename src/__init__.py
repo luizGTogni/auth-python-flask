d77902ee70aa435c3cab0dbc85ed2cf0393eb2d8
Luiz Togni <contato.togni@gmail.com>
2025-07-16 15:51:58 -0300

    feat: add bcrypt

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,9 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv()
 
@@ -13,3 +15,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DB_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+from src import routes
