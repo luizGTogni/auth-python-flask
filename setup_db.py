e13af7c403a37f2be953585c9e11c322b257a13e
Luiz Togni <contato.togni@gmail.com>
2025-07-17 10:04:27 -0300

    refactor: add user admin with role permission

--- setup_db.py
+++ setup_db.py
@@ -5,7 +5,7 @@ with app.app_context():
     db.drop_all()
     db.create_all()
     password_hashed = bcrypt.generate_password_hash("123456").decode("utf-8")
-    admin = models.user.User(username="admin", password=password_hashed)
+    admin = models.user.User(username="toogni", password=password_hashed, role="admin")
     db.session.add(admin)
     db.session.commit()
     print("Database created!")
