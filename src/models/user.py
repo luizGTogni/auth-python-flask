6d0de51fcf4e03f3672944238998b7029d462dc0
Luiz Togni <contato.togni@gmail.com>
2025-07-17 10:04:02 -0300

    feat: add role column to User model

--- src/models/user.py
+++ src/models/user.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(30), nullable=False, default="user")
 
     def to_dict(self):
         return { "id": self.id, "username": self.username }
